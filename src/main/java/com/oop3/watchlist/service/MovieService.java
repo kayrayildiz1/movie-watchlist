579e843408c0e961104738e7f809443a5b0557b8
kayrayildiz1 <723073@student.inholland.nl>
2025-07-17 20:43:17 +0300

    Update MovieService.java

--- src/main/java/com/oop3/watchlist/service/MovieService.java
+++ src/main/java/com/oop3/watchlist/service/MovieService.java
@@ -11,6 +11,11 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Service class for handling movie-related operations.
+ * Integrates with OMDb and TMDb APIs to enrich movie data.
+ */
+
 @Service
 public class MovieService {
 
@@ -18,16 +23,30 @@ public class MovieService {
     private final OmdbClient omdbClient;
     private final TmdbClient tmdbClient;
 
+   
     @Autowired
     public MovieService(MovieRepository movieRepository, OmdbClient omdbClient, TmdbClient tmdbClient) {
         this.movieRepository = movieRepository;
         this.omdbClient = omdbClient;
         this.tmdbClient = tmdbClient;
     }
+    
+    /**
+     * Returns a list of all movies in the database.
+     *
+     * @return list of movies
+     */
+
 
     public List<Movie> getAllMovies() {
         return movieRepository.findAll();
     }
+      /**
+     * Adds a new movie and enriches it with OMDb metadata.
+     *
+     * @param movie the movie to add
+     * @return the saved movie
+     */
 
     public Movie addMovie(Movie movie) {
         JsonObject omdbData = omdbClient.fetchMovie(movie.getTitle());
@@ -46,14 +65,33 @@ public class MovieService {
 
         return movieRepository.save(movie);
     }
+    /**
+     * Deletes a movie by its ID.
+     *
+     * @param id the ID of the movie to delete
+     */
 
     public void deleteMovie(Long id) {
         movieRepository.deleteById(id);
     }
+        /**
+     * Finds a movie by its ID.
+     *
+     * @param id the ID of the movie
+     * @return the movie if found, otherwise Optional.empty()
+     */
+
 
     public Optional<Movie> getMovieById(Long id) {
         return movieRepository.findById(id);
     }
+        /**
+     * Checks if a movie exists by its ID.
+     *
+     * @param id the ID to check
+     * @return true if the movie exists, false otherwise
+     */
+
 
     public boolean movieExists(Long id) {
         return movieRepository.existsById(id);
