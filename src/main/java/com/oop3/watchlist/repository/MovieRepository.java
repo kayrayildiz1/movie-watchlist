95e9ea25bd91ef13a48d51acd6efc9d79afc5851
kayrayildiz1 <723073@student.inholland.nl>
2025-07-17 20:51:24 +0300

    Update MovieRepository.java

--- src/main/java/com/oop3/watchlist/repository/MovieRepository.java
+++ src/main/java/com/oop3/watchlist/repository/MovieRepository.java
@@ -4,6 +4,11 @@ import com.oop3.watchlist.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+/**
+ * JPA repository interface for Movie entity.
+ * Provides CRUD operations.
+ */
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
     // Spring automatically provides methods like:
